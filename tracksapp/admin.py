01b13e292197cdb38ecaa25eeb9191e700fbc25e
saraahmed55 <ssaarraaahmed55@gmail.com>
2020-12-25 19:54:43 +0200

    add tracks views and url

--- tracksapp/admin.py
+++ tracksapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import tracksapp
+from .models import tracksapp,Category
 
 admin.site.register(tracksapp)
+admin.site.register(Category)
--- tracksapp/models.py
+++ tracksapp/models.py
@@ -4,5 +4,13 @@ from django.db import models
 class tracksapp(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField(max_length=1000)
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name=models.CharField(max_length=25)        
+
+
+    def __str__(self):
+        return self.name
